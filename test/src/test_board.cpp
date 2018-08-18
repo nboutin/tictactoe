7b58813f0f4563072ebb60cecc23f6022e87a589
nboutin <boutwork@gmail.com>
2018-08-18 12:17:48 +0200

    add test board boundaries

--- test/src/test_board.cpp
+++ test/src/test_board.cpp
@@ -22,6 +22,23 @@ TEST_CASE("play two token", "[board]")
     REQUIRE(bn[0][3].is_empty());
 }
 
+TEST_CASE("boundaries", "[board]")
+{
+    Board b;
+
+    auto piece = std::make_unique<Token>(Token::color_e::red);
+    REQUIRE_FALSE(b.play(-1, std::move(piece)));
+
+    piece = std::make_unique<Token>(Token::color_e::yellow);
+    REQUIRE_FALSE(b.play(Board::N_COLUMN, std::move(piece)));
+
+    bool empty = true;
+    for(const auto& c : b.get_board())
+        for(const auto& r : c)
+            empty &= r.is_empty();
+    REQUIRE(empty);
+}
+
 TEST_CASE("full column", "[board]")
 {
     Board b;
