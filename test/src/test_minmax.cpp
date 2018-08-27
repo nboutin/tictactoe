d36a77ef30d9869c4879f225d13961e31415e9a5
nboutin <nicolas.boutin.work@gmail.com>
2018-08-27 17:56:50 +0200

    clean test code

--- test/src/test_minmax.cpp
+++ test/src/test_minmax.cpp
@@ -17,11 +17,11 @@ using namespace std;
 
 TEST_CASE("min", "[minmax]")
 {
-    // ----
-    // ----
-    // r-Y-
-    // r-y-
-    // r-y-
+    // -----
+    // -----
+    // r--Y-
+    // r--y-
+    // r--y-
     SECTION("depth 0")
     {
         const int depth = 0;
@@ -73,20 +73,14 @@ TEST_CASE("min", "[minmax]")
 
         for(int i = 0; i < Board::N_COLUMN; ++i)
         {
-            //            cout << "###########i:" << i << endl;
-
             REQUIRE(game.get_current_player().get_color() == color_e::red);
             CAPTURE(i);
             REQUIRE(game.play(i));
-
-            //            view::View_ASCII v(game.get_board());
-            //            v.display(false);
 
             REQUIRE(minmax.min(game, depth) == expected_min[i]);
 
             for(int j = 0; j < Board::N_COLUMN; ++j)
             {
-                //                cout << "j:" << j << endl;
                 if(i == 0)
                     continue;
 
@@ -95,8 +89,6 @@ TEST_CASE("min", "[minmax]")
                 CAPTURE(j);
                 REQUIRE(game.play(j));
 
-                //                v.display(false);
-
                 REQUIRE(minmax.max(game, depth) == expected_max[i][j]);
                 game.undo();
             }
@@ -104,95 +96,3 @@ TEST_CASE("min", "[minmax]")
         }
     }
 }
-
-// TEST_CASE("eval", "[minmax]")
-//{
-//    SECTION("")
-//    {
-//        Game_P4 game;
-//        game.set_ai(1);
-//
-//        Minmax minmax(game.get_player(1), 0);
-//
-//        //    y---
-//        //    r-y-
-//        //    r-y-
-//        //    rry-
-//        for(auto m : vector<int>{0, 3, 0, 3, 0, 3, 1, 0})
-//            game.play(m);
-//
-//        game.play(0);
-//
-//        game.play(0);
-//        REQUIRE(minmax.evaluate(game) == -3);
-//        game.unplay(0);
-//
-//        game.play(1);
-//        REQUIRE(minmax.evaluate(game) == -3);
-//        game.unplay(1);
-//
-//        game.play(2);
-//        REQUIRE(minmax.evaluate(game) == -3);
-//        game.unplay(2);
-//
-//        game.play(3);
-//        REQUIRE(minmax.evaluate(game) == LOOSE_POINT);
-//        REQUIRE(game.is_finished());
-//        game.unplay(3);
-//        REQUIRE_FALSE(game.is_finished());
-//
-//        game.play(4);
-//        view::View_ASCII v(game.get_board());
-//        v.display(false);
-//        REQUIRE(minmax.evaluate(game) == -3);
-//        game.unplay(4);
-//
-//        //        view::View_ASCII v(game.get_board());
-//        //        v.display(false);
-//    }
-//}
-
-// TEST_CASE("compute", "[minmax]")
-//{
-//    Game_P4 game;
-//    game.set_ai(1);
-//
-//    //    y---
-//    //    r-y-
-//    //    r-y-
-//    //    rry-
-//    for(auto m : vector<int>{0, 3, 0, 3, 0, 0, 1, 3})
-//        game.play(m);
-//
-//    //    SECTION("depth 0")
-//    //    {
-//    //        Minmax minmax(game.get_player(1), 0);
-//    //        REQUIRE(minmax.compute(game) == 3);
-//    //    }
-//
-//    SECTION("depth 1")
-//    {
-//        view::View_ASCII v(game.get_board());
-//        v.display(false);
-//        cout << "start position" << endl;
-//
-//        Minmax minmax(game.get_player(1), 1);
-//        REQUIRE(minmax.compute(game) == 3);
-//    }
-//}
-
-// TEST_CASE("debug", "")
-//{
-//    Game_P4 game;
-//    game.set_ai(1);
-//    Minmax minmax(game.get_player(1), 0);
-//
-//    for(auto m :
-//        vector<int>{2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 6,
-//        5, 5, 5, 5, 5, 0, 5}) game.play(m);
-//
-//    view::View_ASCII v(game.get_board());
-//    v.display(false);
-//
-//    REQUIRE(minmax.evaluate(game) == MIN);
-//}
