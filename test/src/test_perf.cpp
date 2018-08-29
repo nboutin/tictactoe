e019f6d89fa1989da12a82ededb9b0726684ac33
nboutin <boutwork@gmail.com>
2018-08-29 23:05:28 +0200

    updated test_perf

--- test/src/test_perf.cpp
+++ test/src/test_perf.cpp
@@ -11,6 +11,10 @@ using namespace p4;
 using namespace ai;
 using namespace std;
 
+constexpr auto C1   = 1000;
+constexpr auto C2   = 100;
+constexpr auto E1   = 0.05;
+
 TEST_CASE("perf")
 {
     SECTION("depth 0")
@@ -18,7 +22,7 @@ TEST_CASE("perf")
         Game_P4 game;
         Minmax minmax(game.get_player(1), 0);
         vector<uint32_t> durations;
-        for(int i = 0; i < 100; ++i)
+        for(int i = 0; i < C1; ++i)
         {
             auto start = std::chrono::high_resolution_clock::now();
             minmax.compute(game);
@@ -28,7 +32,7 @@ TEST_CASE("perf")
             durations.push_back(d);
         }
         auto average = std::accumulate(durations.begin(), durations.end(), 0) / durations.size();
-        REQUIRE(average == Approx(chrono::microseconds(200).count()).epsilon(0.10));
+        REQUIRE(average == Approx(chrono::microseconds(180).count()).epsilon(E1));
     }
 
     SECTION("depth 1")
@@ -36,7 +40,7 @@ TEST_CASE("perf")
         Game_P4 game;
         Minmax minmax(game.get_player(1), 1);
         vector<uint32_t> durations;
-        for(int i = 0; i < 100; ++i)
+        for(int i = 0; i < C1; ++i)
         {
             auto start = std::chrono::high_resolution_clock::now();
             minmax.compute(game);
@@ -46,7 +50,7 @@ TEST_CASE("perf")
             durations.push_back(d);
         }
         auto average = std::accumulate(durations.begin(), durations.end(), 0) / durations.size();
-        REQUIRE(average == Approx(chrono::microseconds(1300).count()).epsilon(0.10));
+        REQUIRE(average == Approx(chrono::microseconds(1285).count()).epsilon(E1));
     }
 
     SECTION("depth 2")
@@ -54,7 +58,7 @@ TEST_CASE("perf")
         Game_P4 game;
         Minmax minmax(game.get_player(1), 2);
         vector<uint32_t> durations;
-        for(int i = 0; i < 100; ++i)
+        for(int i = 0; i < C1; ++i)
         {
             auto start = std::chrono::high_resolution_clock::now();
             minmax.compute(game);
@@ -64,7 +68,7 @@ TEST_CASE("perf")
             durations.push_back(d);
         }
         auto average = std::accumulate(durations.begin(), durations.end(), 0) / durations.size();
-        REQUIRE(average == Approx(chrono::microseconds(9000).count()).epsilon(0.10));
+        REQUIRE(average == Approx(chrono::microseconds(9060).count()).epsilon(E1));
     }
 
     SECTION("depth 3")
@@ -72,7 +76,7 @@ TEST_CASE("perf")
         Game_P4 game;
         Minmax minmax(game.get_player(1), 3);
         vector<uint32_t> durations;
-        for(int i = 0; i < 100; ++i)
+        for(int i = 0; i < C2; ++i)
         {
             auto start = std::chrono::high_resolution_clock::now();
             minmax.compute(game);
@@ -82,7 +86,7 @@ TEST_CASE("perf")
             durations.push_back(d);
         }
         auto average = std::accumulate(durations.begin(), durations.end(), 0) / durations.size();
-        REQUIRE(average == Approx(chrono::microseconds(65000).count()).epsilon(0.10));
+        REQUIRE(average == Approx(chrono::microseconds(63700).count()).epsilon(E1));
     }
 
     SECTION("depth 4")
@@ -90,7 +94,7 @@ TEST_CASE("perf")
         Game_P4 game;
         Minmax minmax(game.get_player(1), 3);
         vector<uint32_t> durations;
-        for(int i = 0; i < 100; ++i)
+        for(int i = 0; i < C2; ++i)
         {
             auto start = std::chrono::high_resolution_clock::now();
             minmax.compute(game);
@@ -100,6 +104,6 @@ TEST_CASE("perf")
             durations.push_back(d);
         }
         auto average = std::accumulate(durations.begin(), durations.end(), 0) / durations.size();
-        REQUIRE(average == Approx(chrono::milliseconds(63500).count()).epsilon(0.10));
+        REQUIRE(average == Approx(chrono::milliseconds(64350).count()).epsilon(E1));
     }
 }
