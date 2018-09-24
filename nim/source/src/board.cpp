6391cc00fe2d35aa66dfb109ee2f9c6e3cc6e78d
nboutin <boutwork@gmail.com>
2018-09-24 23:32:08 +0200

    add nim game

---
+++ nim/source/src/board.cpp
@@ -0,0 +1,15 @@
+
+#include "board.h"
+
+using namespace nim;
+
+bool Board::play(const move_t m)
+{
+    if(m < MOVE_MIN || m > MOVE_MAX || (tokens - m) < TOKEN_MIN)
+        return false;
+
+    tokens -= m;
+    return true;
+}
+
+void Board::unplay(const move_t m) { tokens += m; }
---
+++ nim/source/src/view_ascii.cpp
@@ -0,0 +1,52 @@
+
+#include "view_ascii.h"
+
+#include <iostream>
+
+using namespace nim;
+using namespace std;
+
+View_ASCII::View_ASCII(const Board::grid_t& g) : grid(g), current_player("") {}
+
+void View_ASCII::display(bool clear)
+{
+    if(clear)
+        if(std::system("clear") == -1)
+            return;
+
+    for(auto i = 0; i < Board::TOKEN_COUNT; ++i)
+        cout << std::to_string(i);
+    cout << '\n';
+
+    print_grid(grid);
+    print_history();
+
+    for(const auto& msg : msgs)
+        cout << msg << '\n';
+
+    cout << flush;
+    msgs.clear();
+}
+
+void View_ASCII::message(const std::string& msg) { msgs.push_back(msg); }
+
+void View_ASCII::print_history() const
+{
+    cout << "history:";
+    for(auto h : history)
+    {
+        if(h)
+            cout << (int) h.value();
+        else
+            cout << '-';
+        cout << ",";
+    }
+    cout << '\n';
+}
+
+void View_ASCII::print_grid(const Board::grid_t& g) const
+{
+    for(auto i = 0; i < g; ++i)
+        cout << "|";
+    cout << '\n';
+}
